4109b11980e1b6351244289c38c0250a6a55af00
Kris Hall <dezcont@gmail.com>
2018-11-15 19:26:33 -0600

    added functionality in main.cpp to stay open

--- tut1-window/main.cpp
+++ tut1-window/main.cpp
@@ -12,7 +12,7 @@ using namespace glm;
 int main(){
 	glewExperimental = true;
 	if( !glfwInit() ){
-		std::cerr << "ERROR: Failed to initialize GLFW" << endl;
+		std::cerr << "ERROR: Failed to initialize GLFW" << std::endl;
 		return -1;
 	}
 	
@@ -24,14 +24,25 @@ int main(){
 	GLFWwindow *window;
 	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
 	if( window == NULL ){
-		std::cerr << "ERROR: Failed to open GLFW window" << endl;
+		std::cerr << "ERROR: Failed to open GLFW window" << std::endl;
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 	glewExperimental = true;
 	if( glewInit() != GLEW_OK ){
-		std::cerr << "ERROR: Failed to initialize GLEW" << endl;
+		std::cerr << "ERROR: Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
+		// Drawing code
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while ( (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) && (glfwWindowShouldClose(window) == 0) );
 }
